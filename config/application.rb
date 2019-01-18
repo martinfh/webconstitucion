ab086949d98e7ae07d1a1f918851b4964806adda
martinfh <martinfarnholchalley@gmail.com>
2019-01-17 23:27:02 -0300

    cambie el config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module Constitucion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
