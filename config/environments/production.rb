196b8d049d896365cc906eb1cc61bb2a4147d9a8
martinfh <martinfarnholchalley@gmail.com>
2019-01-26 14:36:22 -0300

    xxx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   config.assets.compile = false
   config.serve_static_files = true
   config.assets.digest = true
-  config.cache.cache_classes = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
