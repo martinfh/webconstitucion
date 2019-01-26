143e884754d370a9e885ddbd78fde0717ad129b3
martinfh <martinfarnholchalley@gmail.com>
2019-01-26 14:29:53 -0300

    xxx

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module Constitucion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-	config.assets.initialize_on_precompile = true
+	config.assets.initialize_on_precompile = false
 	config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
