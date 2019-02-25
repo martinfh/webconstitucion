f72ab7fdb84ac0e52509c4a88c1f03d0d61aad30
martinfh <martinfarnholchalley@gmail.com>
2019-02-25 18:57:40 -0300

    cc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,17 +20,6 @@
 #= require turbolinks_transitions
 #= require_tree .
 
-function myAccFunc() {
-  var x = document.getElementById("demoAcc");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-    x.previousElementSibling.className = 
-    x.previousElementSibling.className.replace(" w3-green", "");
-  }
-}
-
 import * as ActiveStorage from "activestorage"
 ActiveStorage.start()
   // direct_uploads.js
